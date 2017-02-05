9e271b68bea5ff4b1f3c855e3929300d969a512f
cavemanjaw <cavemanjaw@op.pl>
2017-02-05 13:48:42 +0100

    Added header guard and started implementation

--- Complex.cpp
+++ Complex.cpp
@@ -1,1 +1,13 @@
 #include "Complex.h"
+
+template <class T>
+class Complex
+{
+public:
+	Complex(T real, T imag);
+private:
+	Complex& operator+(Complex rhs);
+	Compex& operator-(Compex rhs);
+	T real;
+	T imag;
+};
